c8868cc27d25b108b72fbdabe84178252d2378e1
gouravgouravgupta <gouravgupta7879@gmail.com>
2021-10-30 21:30:38 +0530

    binary search iterative approch

---
+++ Java/Searching/BinarySearch1.java
@@ -0,0 +1,31 @@
+public class BinarySearch1 {
+
+    public int binarySearchRecursively(int[] sortedArray, int key, int low, int high) {
+   
+     int middle = (low + high) / 2;
+     if (high < low) {
+      return -1;
+     }
+   
+     if (key == sortedArray[middle]) {
+      return middle;
+     } else if (key < sortedArray[middle]) {
+      return binarySearchRecursively(sortedArray, key, low, middle - 1);
+     } else {
+      return binarySearchRecursively(sortedArray, key, middle + 1, high);
+     }
+    }
+   
+    
+    public static void main(String[] args) {
+     int[] sortedArray = { 0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9 };
+        int key = 6;
+        
+        BinarySearch binSearch = new BinarySearch();
+           
+           int index = binSearch.binarySearchRecursively(sortedArray, key, 0, sortedArray.length -1);
+           System.out.println("Search element found in location index : " + index);
+           
+    }
+   }
+   
